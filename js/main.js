25a97aa46c9b05826306ffb5333d3199117f46a7
Unknown <olka27forever@gmail.com>
2018-12-22 15:21:13 +0300

    my project commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+
+        }
+    })
+});
+
+
+$(document).ready(function () {
+    $("#scroll").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+
